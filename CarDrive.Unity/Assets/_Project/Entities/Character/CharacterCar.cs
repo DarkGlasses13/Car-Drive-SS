9debc652e6c03cde8c088c5cf18c0d1a4d2179e1
Matroskin_Cat <groznov3@gmail.com>
2023-07-21 18:55:46 +0300

    Driving system final draft

--- CarDrive.Unity/Assets/_Project/Entities/Character/CharacterCar.cs
+++ CarDrive.Unity/Assets/_Project/Entities/Character/CharacterCar.cs
@@ -12,8 +12,6 @@ namespace Assets._Project.Entities.Character
     {
         private Rigidbody _rigidbody;
         private TweenerCore<Vector3, Vector3, VectorOptions> _moveTween;
-        private TweenerCore<Quaternion, Vector3, QuaternionOptions> _rotationTween;
-        private Sequence _changeLineSequence;
 
         public Vector3 Center => transform.position;
         public Quaternion Rotation => transform.rotation;
@@ -30,24 +28,20 @@ namespace Assets._Project.Entities.Character
 
         public void ChangeLine(float line, float duration, float stearAngle)
         {
-            //_changeLineSequence = new Sequence();
             _moveTween?.Kill();
             _moveTween = transform.DOMoveX(line, duration);
-            //_rotationTween = transform.DORotate(shift * stearAngle * Vector3.up, duration / 2).SetLoops(2, LoopType.Yoyo);
             _moveTween?.Play();
         }
 
         public void Accelerate(float acceleration)
         {
             transform.position += transform.forward * acceleration;
-            //_rigidbody.velocity = new(_rigidbody.velocity.x, _rigidbody.velocity.y, acceleration);
         }
 
         public void Die()
         {
             _rigidbody.isKinematic = false;
             _moveTween?.Kill();
-            //_rotationTween?.Kill();
             transform.DOPunchScale(Vector3.one * 2, 0.25f).Play().SetAutoKill(true);
         }
     }
